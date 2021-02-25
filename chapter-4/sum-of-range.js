7e842852f41f40521d04a642999a38133e53a700
peterbuoy <peterbuoy@gmail.com>
2021-02-24 18:47:21 -0800

    implement sum of array and making array from range

---
+++ chapter-4/sum-of-range.js
@@ -0,0 +1,28 @@
+function range(start, end, step = 1) {
+  array = [];
+  if (step < 0) {
+    for (let i = start; i >= end; i += step) {
+      array.push(i);
+    }
+  } else {
+    for (let i = start; i <= end; i += step) {
+    	array.push(i);
+  	}
+  }
+  return array;
+}
+
+function sum(array) {
+  let sum = 0;
+  for (let i of array) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(range(1, 10));
+// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(range(5, 2, -1));
+// → [5, 4, 3, 2]
+console.log(sum(range(1, 10)));
+// → 55
