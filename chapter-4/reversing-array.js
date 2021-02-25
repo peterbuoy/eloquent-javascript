edd6496b08d383d2e4f1c0f68798ba951afe310d
peterbuoy <peterbuoy@gmail.com>
2021-02-24 21:35:07 -0800

    reversed array in place and with a new array

---
+++ chapter-4/reversing-array.js
@@ -0,0 +1,37 @@
+function reverseArray(array) {
+  rArray = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    rArray.push(array[i]);
+  }
+  return rArray;
+}
+
+function reverseArrayInPlace(array) {
+  let i = 0;
+  // even num elements
+  if (array.length % 2 === 0) {
+    for (let i = 0; i <= array.length / 2 - 1; i++) {
+      let temp = array[i];
+      array[i] = array[array.length - 1 - i];;
+      array[array.length - 1 - i] = temp;
+    }
+  } else {
+    for (let i = 0; i < Math.floor(array.length / 2) - 1; i++) {
+      let temp = array[i];
+      array[i] = array[array.length - 1 - i];;
+      array[array.length - 1 - i] = temp;
+    }
+  }
+   return array;
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
+let arrayValue2 = [1, 2, 3, 4, ];
+reverseArrayInPlace(arrayValue2);
+console.log(arrayValue2);
+// → [4, 3, 2, 1]
